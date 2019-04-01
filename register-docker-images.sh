1fc4e4663876234888b441537a122d4b26db4814
Noel Yap <noel.yap+github.com@gmail.com>
2019-04-01 14:06:35 -0700

    `docker login` command fixed.

--- register-docker-images.sh
+++ register-docker-images.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -evx
 
+# Creates Docker image and registers it in ECR.
+
+aws_account_id=$(aws sts get-caller-identity --output text --query Account)
+$(aws ecr get-login --no-include-email --region us-east-1 | sed 's|https://||')
+
+register-docker-image() {
+  dockerfile=$1
+  docker_tag=$(echo ${dockerfile} | sed -e 's|.*Dockerfile.||')
+
+  docker build -f ${dockerfile} --tag ${docker_tag} . &&
+      docker tag ${docker_tag} ${aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/packager
+}
+
 for docker_tag in packager{,.node}
 do
-  $(dirname $0)/register-docker-image.sh Dockerfile.${docker_tag}
+  register-docker-image $(dirname $0)/Dockerfile.${docker_tag}
 done
 
+docker push ${aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/packager
+docker logout
