664395f9b7148add34be413ec7feb12ce6c57fdc
Noel Yap <noel.yap+github.com@gmail.com>
2019-04-01 14:47:59 -0700

    Region set to `us-east-2`.

--- register-docker-images.sh
+++ register-docker-images.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -evx
 
-# Creates Docker image and registers it in ECR.
+# Creates Docker images and registers it in ECR.
+
+region=us-east-2
 
 aws_account_id=$(aws sts get-caller-identity --output text --query Account)
-$(aws ecr get-login --no-include-email --region us-east-1 | sed 's|https://||')
+$(aws ecr get-login --no-include-email --region ${region}| sed 's|https://||')
 
 register-docker-image() {
   dockerfile=$1
   docker_tag=$(echo ${dockerfile} | sed -e 's|.*Dockerfile.||')
 
   docker build -f ${dockerfile} --tag ${docker_tag} . &&
-      docker tag ${docker_tag} ${aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/packager
+      docker tag ${docker_tag} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/packager
 }
 
 for docker_tag in packager{,.node}
@@ -18,5 +20,5 @@ do
   register-docker-image $(dirname $0)/Dockerfile.${docker_tag}
 done
 
-docker push ${aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/packager
+docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/packager
 docker logout
